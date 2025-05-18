e4bca86ce3b63deafecee64c802da12be9662ce5
keyvan <keyvan.attarian@polytechnique.edu>
2025-05-18 15:24:37 +0200

    rassification du code des models + embedding pour channel

---
+++ models/distilBert.py
@@ -0,0 +1,23 @@
+from transformers import DistilBertTokenizer, DistilBertModel
+import torch
+import torch.nn as nn
+
+class DistilBert(nn.Module):
+    def __init__(self, text_model_name='distilbert-base-uncased', freeze=True, freeze_last_layers=False):
+
+        self.tokenizer = DistilBertTokenizer.from_pretrained(text_model_name)
+        self.text_encoder = DistilBertModel.from_pretrained(text_model_name)
+        if freeze:
+            for param in self.text_encoder.parameters():
+                param.requires_grad = False
+            if not freeze_last_layers:
+                for name, param in list(self.text_encoder.named_parameters())[-2:]:
+                    param.requires_grad = True
+        self.dim = self.text_encoder.config.hidden_size
+
+    def forward(self, input_text,device):
+        inputs = self.tokenizer(input_text, return_tensors="pt", padding=True, truncation=True, max_length=30)
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+        outputs = self.text_encoder(**inputs)
+        title_feat = outputs.last_hidden_state[:, 0, :]  # [CLS] token
+        return title_feat
--- models/multimodal.py
+++ models/multimodal.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from transformers import DistilBertTokenizer, DistilBertModel
 from models.dinov2 import DinoV2Finetune
+from models.distilBert import DistilBert
 
 class MultiModalRegressor(nn.Module):
     def __init__(self, text_model_name='distilbert-base-uncased', freeze_dino=True):
@@ -12,21 +13,21 @@ class MultiModalRegressor(nn.Module):
         self.image_embedding_dim = self.image_encoder.dim
 
         # --- Text encoder (DistilBERT)
-        self.tokenizer = DistilBertTokenizer.from_pretrained(text_model_name)
-        self.text_encoder = DistilBertModel.from_pretrained(text_model_name)
-        for param in self.text_encoder.parameters():
-            param.requires_grad = False
-        # Unfreeze the last two layers of the backbone
-        for name, param in list(self.text_encoder.named_parameters())[-2:]:
-            param.requires_grad = True
-        self.text_embedding_dim = self.text_encoder.config.hidden_size
+        self.text_encoder = DistilBert(text_model_name=text_model_name)
+        self.text_embedding_dim = self.text_encoder.dim
+
+        channel_embedding_dim = 8
+        self.channel_embedding = nn.Embedding(1, channel_embedding_dim)
+
+        self.max_year = 2025
+        self.min_year = 2011
 
         self.input_dim = self.image_embedding_dim + self.text_embedding_dim
         self.tabular_dim = 2
         self.droupout = 0.2
 
         # --- Fusion + MLP
-        self.img_projector = nn.Sequential(
+        self.image_projector = nn.Sequential(
             nn.Linear(self.image_embedding_dim, 256),
             nn.ReLU(),
             nn.Dropout(self.droupout)
@@ -36,7 +37,7 @@ class MultiModalRegressor(nn.Module):
             nn.ReLU(),
             nn.Dropout(self.droupout)
         )
-        self.fc = nn.Sequential(
+        self.reg_head = nn.Sequential(
             nn.Dropout(self.droupout),
             nn.Linear(512+self.tabular_dim, 1),
             nn.ReLU(),
@@ -45,6 +46,7 @@ class MultiModalRegressor(nn.Module):
         print("shape of image encoder: ", self.image_embedding_dim)
         print("shape of text encoder: ", self.text_embedding_dim)
         print("shape of fusion: ", self.input_dim)
+
 
     def forward(self, x):
         image_tensor = x["image"]
@@ -56,23 +58,18 @@ class MultiModalRegressor(nn.Module):
 
         # --- Image features
         image_feat = self.image_encoder({"image": image_tensor}).squeeze(-1)  # [B, dim]
+        image_feat = self.image_projector(image_feat)
 
         # --- Title features
-        title_inputs = self.tokenizer(title_texts, return_tensors="pt", padding=True, truncation=True, max_length=30)
-        title_inputs = {k: v.to(device) for k, v in title_inputs.items()}
-        title_outputs = self.text_encoder(**title_inputs)
-        title_feat = title_outputs.last_hidden_state[:, 0, :]  # [CLS] token
+        title_feat = self.text_encoder(title_texts)
+        title_feat = self.text_projector(title_feat,device)
 
-        # --- Description features
-        """desc_inputs = self.tokenizer(desc_texts, return_tensors="pt", padding=True, truncation=True, max_length=200)
-        desc_inputs = {k: v.to(device) for k, v in desc_inputs.items()}
-        desc_outputs = self.text_encoder(**desc_inputs)
-        desc_feat = desc_outputs.last_hidden_state[:, 0, :]"""
+        # --- Tabular features
+        channel_feat = self.channel_embedding(channel)
+        year_feat = (year - self.min_year) / (self.max_year - self.min_year)
 
-        # --- Fusion & regression
-        image_feat = self.img_projector(image_feat)
-        title_feat = self.text_projector(title_feat)
-        x = torch.cat([image_feat, title_feat, channel.float(), year.float()], dim=1)
-        x = self.fc(x)
+        x = torch.cat([image_feat, title_feat, channel_feat, year_feat], dim=1)
+        x = self.reg_head(x)
+        
         return x
 
